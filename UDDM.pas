9e9d3f673b4313f30cbc87fee70bb3e17adc5613
likemetoplayforyou <likemetoplayforyou@gmail.com>
2020-02-20 20:40:07 +1000

    Load bitmaps from folder and get them by unit type

--- UDDM.pas
+++ UDDM.pas
@@ -4,16 +4,19 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, ExtCtrls,
+  Dialogs, ExtCtrls, Generics.Collections,
   UDEnvironment, UDUnit, pngimage;
 
 type
   TDDM = class(TForm, IDEnvironment)
     imgSwordsMan: TImage;
+    procedure FormCreate(Sender: TObject);
   private
     { Private declarations }
+    FDUnitBitmaps: TDictionary<TDUnitType, TBitmap>;
   public
     function GetImageByUnitType(AUnitType: TDUnitType): TImage;
+    function GetBitmapByUnitType(AUnitType: TDUnitType): TBitmap;
   end;
 
 var
@@ -25,8 +28,33 @@ implementation
 
 { TDDM }
 
+procedure TDDM.FormCreate(Sender: TObject);
+var
+  duInfo: TDUnitInfo;
+  bitmap: TBitmap;
+begin
+  inherited;
+  FDUnitBitmaps :=
+    TObjectDictionary<TDUnitType, TBitmap>.Create([doOwnsValues]);
+
+  for duInfo in DUNIT_INFOS do begin
+    bitmap := TBitmap.Create;
+    FDUnitBitmaps.Add(duInfo.UnitType, bitmap);
+
+    bitmap.LoadFromFile('Images/' + duInfo.Name + '.bmp');
+  end;
+end;
+
+
+function TDDM.GetBitmapByUnitType(AUnitType: TDUnitType): TBitmap;
+begin
+  Result := FDUnitBitmaps[AUnitType];
+end;
+
+
 function TDDM.GetImageByUnitType(AUnitType: TDUnitType): TImage;
 begin
+
   case AUnitType of
     utSwordsMan: Result := imgSwordsMan;
     else
